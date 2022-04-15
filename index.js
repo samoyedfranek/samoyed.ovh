89efb97aa38b32571f30f580694a1bd176f1163f
Mój Komp <fhadzlik9@gmail.com>
2022-04-15 17:59:52 +0200

    master

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.use(express.static(path.join(__dirname + "/strona")));
 
 io.on("connection", function (socket) {
     socket.on("newuser", function (username) {
-        socket.broadcast.emit("update", "Użytkownik" + username + " dołączył do czatu")
+        socket.broadcast.emit("update", "Użytkownik " + username + " dołączył do czatu")
     })
     socket.on("exiting", function (username) {
-        socket.broadcast.emit("update", "Użytkownik" + username + " opuścił czat")
+        socket.broadcast.emit("update", "Użytkownik " + username + " opuścił czat")
     })
     socket.on("chat", function (message) {
         socket.broadcast.emit("chat", message)
     })
 })
 
-server.listen(process.env.PORT || 3000, () => console.log("Server is running..."))
+server.listen(process.env.PORT || 3000, () => console.log("Server działa..."))
--- strona/js/sidebarbuttons.js
+++ strona/js/sidebarbuttons.js
@@ -1,10 +1,85 @@
-if (location.href.includes("#czat")) {
-    const app = document.querySelector(".home_content");
-    app.innerHTML = `<iframe width="100%" height="100%" frameBorder="0" src="chat.html"></iframe>`;
+function dom() {
+    if (location.href.includes("#dom")) {
+        const app = document.querySelector(".home_content");
+        app.innerHTML = `Strona jest nadal w trakcie budowy a to jest tylko wersja poglądowa`;
+    }
+    addEventListener("hashchange", e => {
+        if (location.href.includes("#dom")) {
+            const app = document.querySelector(".home_content");
+            app.innerHTML = `Strona jest nadal w trakcie budowy a to jest tylko wersja poglądowa`;
+        }
+    });
 }
-addEventListener("hashchange", e => {
+
+function skracacz() {
+    if (location.href.includes("#skracacz")) {
+        const app = document.querySelector(".home_content");
+        app.innerHTML = `W trakcie budowy`;
+    }
+    addEventListener("hashchange", e => {
+        if (location.href.includes("#skracacz")) {
+            const app = document.querySelector(".home_content");
+            app.innerHTML = `W trakcie budowy`;
+        }
+    });
+}
+
+function pastebin() {
+    if (location.href.includes("#pastebin")) {
+        const app = document.querySelector(".home_content");
+        app.innerHTML = `W trakcie budowy`;
+    }
+    addEventListener("hashchange", e => {
+        if (location.href.includes("#pastebin")) {
+            const app = document.querySelector(".home_content");
+            app.innerHTML = `W trakcie budowy`;
+        }
+    });
+}
+
+function papa() {
+    if (location.href.includes("#2137")) {
+        const app = document.querySelector(".home_content");
+        app.innerHTML = `W trakcie budowy`;
+    }
+    addEventListener("hashchange", e => {
+        if (location.href.includes("#2137")) {
+            const app = document.querySelector(".home_content");
+            app.innerHTML = `W trakcie budowy`;
+        }
+    });
+}
+
+function czat() {
     if (location.href.includes("#czat")) {
         const app = document.querySelector(".home_content");
         app.innerHTML = `<iframe width="100%" height="100%" frameBorder="0" src="chat.html"></iframe>`;
     }
-});
+    addEventListener("hashchange", e => {
+        if (location.href.includes("#czat")) {
+            const app = document.querySelector(".home_content");
+            app.innerHTML = `<iframe width="100%" height="100%" frameBorder="0" src="chat.html"></iframe>`;
+        }
+    });
+}
+
+function stats() {
+    if (location.href.includes("#statystyki")) {
+        const app = document.querySelector(".home_content");
+        app.innerHTML = `W trakcie budowy`;
+    } 
+    addEventListener("hashchange", e => {
+        if (location.href.includes("#statystyki")) {
+            const app = document.querySelector(".home_content");
+            app.innerHTML = `W trakcie budowy`;
+        }
+    });
+}
+
+
+dom()
+skracacz()
+pastebin()
+papa()
+czat()
+stats()
