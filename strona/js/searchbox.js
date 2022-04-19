63fb89f1ddb0895091a6a916ac46edc3de69be68
Franek <78372133+samoyedgaming@users.noreply.github.com>
2022-04-19 16:03:15 +0000

    master

--- strona/js/searchbox.js
+++ strona/js/searchbox.js
@@ -2,6 +2,6 @@ document.querySelector("#szukaj").addEventListener("keypress", function (e) {
     if (e.key === 'Enter') {
         const y = document.getElementById('szukaj').value;
         const x = y.replaceAll(/ /g, "+")
-        window.location = "https://letmegooglethat.com/?q=" + x;
+        window.location = "https://www.google.com/search?q=" + x;
     }
 })
