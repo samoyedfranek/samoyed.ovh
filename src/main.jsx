e00858d38ccf63efeff816579cfdbf5af0db811b
Fryncu <fhadzlik9@gmail.com>
2026-01-13 19:19:44 +0100

    rebuild

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "./pages/homeSite";
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
+);
